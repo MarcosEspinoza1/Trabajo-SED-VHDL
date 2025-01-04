bfc4d1497121abc6b42b14e2b60fb1c282b3aa3e
MarcosEspinoza1 <163344914+MarcosEspinoza1@users.noreply.github.com>
2025-01-04 21:06:42 +0100

    Actualizada game_timer.vhd
    Añadida señal timer_active

--- trabajo1.srcs/sources_1/new/game_timer.vhd
+++ trabajo1.srcs/sources_1/new/game_timer.vhd
@@ -37,6 +37,7 @@ architecture Behavioral of game_timer is
     constant CLOCK_FREQ : unsigned(26 downto 0) := to_unsigned(100_000_000, 27); -- Frecuencia del reloj (100 MHz)
     signal clk_counter  : unsigned(26 downto 0) := (others => '0');              -- Contador de ciclos de reloj
     signal second_count : unsigned(13 downto 0) := (others => '0');              -- Contador de segundos
+    signal timer_active : std_logic := '1';                                      -- Control interno para detener el temporizador  
 begin
 
     process(clk, rst)
@@ -45,8 +46,9 @@ begin
             clk_counter <= (others => '0');
             second_count <= (others => '0');
             time_up <= '0';
+            timer_active <= '1';
         elsif rising_edge(clk) then
-            if enable = '1' then
+            if enable = '1' and timer_active = '1' then
                 -- Incrementa el contador de ciclos
                 if clk_counter = CLOCK_FREQ - 1 then
                     clk_counter <= (others => '0');         -- Reinicia el contador de ciclos
@@ -58,8 +60,7 @@ begin
                 -- Comprueba si se alcanzó el límite
                 if second_count = limit then
                     time_up <= '1';                        -- Indica que el tiempo ha terminado
-                else
-                    time_up <= '0';                        -- Tiempo aún en curso
+                    timer_active <= '0';                   -- Detiene el temporizador                 
                 end if;
             end if;
         end if;
